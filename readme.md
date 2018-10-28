b96bff8b4c32fd964f6d679fcff741e8b1dfcc1d
Gustavo Fenilli <gustavofenilli@hotmail.com>
2018-10-27 22:21:00 -0300

    Primeiro Commit

---
+++ readme.md
@@ -0,0 +1,101 @@
+# CEPFinder
+
+Pacote que use a API do ViaCEP para busca de logradouro usando CEP.
+
+## Começando
+
+Pra começar a usar este pacote tenha certeza de ter instalado composer na sua maquina,
+e que seu projeto siga os padrões de autoload da psr-4.
+
+### Pre Requisitos
+
+[Composer] (https://getcomposer.org/download/)
+
+### Instalação
+
+Um passo a passo de como fazer pra funcionar no seu projeto
+
+Instale o pacote dentro do seu projeto usando
+
+```
+composer require https://github.com/GustavoFenilli/CEPFinder.git
+```
+
+Com o projeto ja registrado no seu autoload
+
+```
+use GustavoFenilli\CEPFinder\CEPFinder;
+
+$cepFinder = new CEPFinder();
+
+$cep = '00000000';
+if($cepFinder->consult($cep) == 200){
+    $cepFinder->getResultJson();
+}else{
+    throw new \Exception("Erro ao buscar no cep");
+}
+```
+
+A resposta pode ser recebida em JSON ou XML nos formatos
+
+```
+// getResultJson()
+
+{
+    "cep":"00000-000",
+    "logradouro":"Rua Teste De Resultado",
+    "complemento":"",
+    "bairro":"Bairro Teste De Resultado",
+    "localidade":"Teste",
+    "uf":"TT",
+    "unidade":"",
+    "ibge":"0000000",
+    "gia":"0000"
+}
+
+// getResultXML() retorna um objeto de \SimpleXMLElement
+
+```
+
+## Uso Avançado
+
+É possivel modificar ou ver os valores recebidos separadamente.
+
+```
+use GustavoFenilli\CEPFinder\CEPFinder;
+
+// É possivel instanciar o objeto CEPFinder ja com o CEP desejado
+$cepFinder = new CEPFinder(00000000);
+if($cepFinder->consult() != 200){
+    throw new \Exception("Erro ao buscar no cep");
+}
+
+/* Ou em variavel e usar no consult, tenha em mente que usando dentro do consult
+** o valor tem prioridade em cima da instancia do objeto, porem ele não sobrescreve
+** o valor do objeto
+*/
+$cep = '00000000';
+if($cepFinder->consult($cep) != 200){
+    throw new \Exception("Erro ao buscar no cep");
+}
+
+// Para pegar valores separadamente use get{NomeAtributo}()
+$cep = $cepFinder->getCep();
+// 00000000
+
+// Para modificar valores depois de recebidos use set{NomeAtributo}($valor)
+$cepFinder->setCep(11111111);
+// 11111111
+
+```
+
+Todo e qualquer metodo ou propriedade errada, vai retornar uma \Exception indicando o problema.
+
+
+## Autores
+
+* **Gustavo Fenilli** - *Criador* - [GustavoFenilli](https://github.com/GustavoFenilli)
+
+## License
+
+Este projeto esta licenciado dentro da licença MIT - Veja em [LICENSE.md](LICENSE.md) arquivo para mais detalhes
---
+++ src/CEPFinder.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace GustavoFenilli\CEPFinder;
+
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class CEPFinder
+{
+    private $client;
+
+    private $apiResult;
+
+    protected $cep;
+    protected $logradouro;
+    protected $complemento;
+    protected $bairro;
+    protected $localidade;
+    protected $uf;
+    protected $unidade;
+    protected $ibge;
+    protected $gia;
+
+    public function __construct($cep = null){
+        if($cep != null) $this->cep = $cep;
+
+        $this->client = new Client();
+    }
+
+    public function consult($cep = null)
+    {
+        if(isset($this->cep)){
+            $cepEscoped = $this->cep;
+        }
+        
+        if($cep != null){
+            $cepEscoped = $cep;
+        }
+
+        try{
+            $result = $this->client->get("viacep.com.br/ws/$cepEscoped/json/");
+        }catch(GuzzleException $exception){
+            return $exception->getResponse()->getStatusCode();
+        }
+        $this->apiResult = json_decode($result->getBody(), true);
+
+        foreach($this->apiResult as $key => $value){
+            if(property_exists($this, $key)){
+                $this->$key = $value;
+            }
+        }
+
+        return $result->getStatusCode();
+    }
+
+    public function getResultJson()
+    {
+        return json_encode($this->apiResult);
+    }
+
+    public function getResultXML()
+    {
+        $xml = new \SimpleXMLElement("<?xml version=\"1.0\"?><localidade></localidade>");
+        $this->arrayToXml($this->apiResult, $xml);
+
+        return $xml;
+    }
+
+    public function __set($name, $value)
+    {
+        if(!property_exists($this, $name)){
+            throw new \Exception("Propriedade $name não existe");
+        }
+
+        parent::__set();
+    }
+
+    public function __get($name)
+    {
+        if(!property_exists($this, $name)){
+            throw new \Exception("Propriedade $name não existe");
+        }
+
+        parent::__get();
+    }
+
+    public function __call($method, $arguments)
+    {
+        $resultPreg = $this->getArrayFromMethodCall($method);
+
+        if(!sizeof($resultPreg) > 1){
+            throw new \Exception("Metodo $method não existe");
+        }
+
+        if($resultPreg[0] == 'get'){
+            return $this->getAttribute($resultPreg[1]);
+        }else if($resultPreg[0] == 'set'){
+            return $this->setAttribute($resultPreg[1], $arguments);
+        }
+
+        throw new \Exception("Metodo $method não existe");
+    }
+
+    private function getArrayFromMethodCall($method)
+    {
+        return explode(' ', preg_replace("/(([a-z])([A-Z])|([A-Z])([A-Z][a-z]))/", "\\2\\4 \\3\\5", $method));
+    }
+
+    private function getAttribute($attribute)
+    {
+        $attribute = $this->lowerCaseAttribute($attribute);
+        return $this->{$attribute};
+    }
+
+    private function setAttribute($attribute, $arguments)
+    {
+        if(sizeof($arguments) > 1){
+            throw new \Exception("Este metodo so aceita um argumento");
+        }
+
+        $value = $arguments[0];
+        $attribute = $this->lowerCaseAttribute($attribute);
+        $this->{$attribute} = $value;
+    }
+
+    private function lowerCaseAttribute($attribute)
+    {
+        return mb_strtolower($attribute);
+    }
+
+    private function arrayToXml($array, &$xml) {
+        foreach($array as $key => $value) {
+            if(is_array($value)) {
+                if(!is_numeric($key)){
+                    $subnode = $xml->addChild("$key");
+                    array_to_xml($value, $subnode);
+                }else{
+                    $subnode = $xml->addChild("item$key");
+                    array_to_xml($value, $subnode);
+                }
+            }else {
+                $xml->addChild("$key",htmlspecialchars("$value"));
+            }
+        }
+    }
+}
